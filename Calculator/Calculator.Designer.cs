5b85c3f0009e6ce8991445638d201304faeb229d
Coolsnake8 <foxtailrb@gmail.com>
2022-11-08 16:43:58 -0700

    Changed colors of control panel bar and background.

--- Calculator/Calculator.Designer.cs
+++ Calculator/Calculator.Designer.cs
@@ -29,12 +29,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.ControlBarPanel = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // ControlBarPanel
+            // 
+            this.ControlBarPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.ControlBarPanel.Location = new System.Drawing.Point(0, 0);
+            this.ControlBarPanel.Name = "ControlBarPanel";
+            this.ControlBarPanel.Size = new System.Drawing.Size(400, 40);
+            this.ControlBarPanel.TabIndex = 0;
+            this.ControlBarPanel.BackColor = Color.FromArgb(51, 51, 51);
+            // 
+            // Calculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(400, 600);
+            this.Controls.Add(this.ControlBarPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Calculator";
             this.Text = "Calculator";
+            this.BackColor = Color.FromArgb(153, 153, 153);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Panel ControlBarPanel;
     }
 }
